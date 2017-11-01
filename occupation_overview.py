3949524296a756ec77ca0f1768bb5de0bdf7b762
John Verghese <john1.verghese@gmail.com>
2017-11-01 19:42:42 -0400

    Skeleton of page.

--- occupation_overview.py
+++ occupation_overview.py
@@ -1,17 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from jinja2 import Environment, PackageLoader, select_autoescape
 import json
 app = Flask(__name__)
 
+# TODO:
+#link to environment to use
+from add_spaces import add_spaces
 
 @app.route('/')
-def hello_world():
-    return "Hello, World!"
+def occ_overview():
+    data = query_data()
+    return render_template('occ_overview.html', occupation=data['occupation'])
 
 def query_data():
     f = open('sample_response.txt', 'r')
     response = f.read()
     response = response.replace('\n', '')
     data = json.loads(response)
-    print(data)
+    return data
 
-query_data()
+print(query_data())
