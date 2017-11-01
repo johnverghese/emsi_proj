876746bc24898f85786e5e7aedd5852b0ecd65c7
John Verghese <john1.verghese@gmail.com>
2017-11-01 18:37:14 -0400

    Cleaned up the JSON.

--- occupation_overview.py
+++ occupation_overview.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import json
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+
+def query_data():
+    f = open('sample_response.txt', 'r')
+    response = f.read()
+    response = response.replace('\n', '')
+    data = json.loads(response)
+    print(data)
+
+query_data()
